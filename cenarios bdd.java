908b04d633b3f336922b18b73baea379213a0064
Bárbara Ann Lima <71350903+Babiplima@users.noreply.github.com>
2021-07-20 11:35:30 -0300

    Update cenarios bdd.java

--- cenarios bdd.java
+++ cenarios bdd.java
@@ -3,20 +3,58 @@ História do usuário
 
 Sendo eu um usuário do plano de saúde Unimed Coop 
 Quero acessar o Guia Médico  
-Para pesquisar médico no Rio de Janeiro e São Paulo
+Para buscar médicos no Rio de Janeiro 
 
-Cenario: Acessar Guia Médico para pesquisa em Rio de Janeiro 
 
-    Dado que acesso a página da Unimed Cooperativa
-    Quando acesso o Guia Médico pelo botão Consulte agora
-    Então sou redirecionada para página com abas Busca rápida e Busca detalhada validando
-    resultados por Especialidade e Cidade Rio de Janeiro
+Dado que o usuario acessar a página da Unimed Cooperativa atraves da url "http://www.unimed.coop.br/"
 
-Cenario: Acessar Guia Médico para pesquisa em São Paulo não apresentação de resultados
+Quando visualiza a tela principal
 
-   Dado que acesso a página da Unimed Cooperativa
-   Quando acesso o Guia Médico pelo botão Consulte agora
-   Então sou redirecionada para página com abas Busca rápida e Busca detalhada validando
-   resultados por Especialidade e Cidade de São Paulo 
+E clicar no botão Consulte agora
+
+Então será redirecionado para a tela Encontre um médico
+
+E clicar na aba Busca detalhada
+
+E selecionar o estado Rio de Janeiro no listbox Estado
+
+E clicar no botão pesquisar
+
+Então a aplicação redireciona o usuário para página de resultados
+
+E validar resultados que contenha tipo de Especialidade
+
+E validar resultados que contenha nome da Cidade
+
+
+
+Cenário: Acessar Guia Médico para pesquisa em São Paulo não apresentação de resultados
+
+
+Dado que o usuário acessa a página da Unimed Cooperativa atraves da url "http://www.unimed.coop.br/"
+
+Quando visualiza a tela principal
+
+E clicar no botão Consulte agora
+
+Então será redirecionado para a tela Encontre um médico
+
+E clicar na aba Busca detalhada
+
+E selecionar o estado Rio de Janeiro no listbox Estado
+
+E clicar no botão pesquisar
+
+Então a aplicação redireciona o usuário para página de resultados
+
+E validar a não existência de resultados que contenha nome da Cidade São Paulo
+
+E clicar no botão Ver mais resultados
+
+E validar a não existência de resultados que contenha nome da Cidade São Paulo
+
+E clicar no botão Ver mais resultados
+
+E validar a não existência de resultados que contenha nome da Cidade São Paulo
 
 
